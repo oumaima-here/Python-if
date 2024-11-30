c1305c5e5e73d9288bcb939d6ac69e5ce3c72df9
oumaima-here <oumaimaelhaddaouy@gmail.com>
2024-11-30 16:05:17 +0100

    commit

---
+++ if.py
@@ -0,0 +1,14 @@
+years = [2000, 2016, 2015, 2004]
+ 
+for year in years:
+    
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print(f"{year} is a leap year")
+            else:
+                print(f"{year} is not a leap year")
+        else:
+            print(f"{year} is a leap year")
+    else:
+        print(f"{year} is not a leap year")
